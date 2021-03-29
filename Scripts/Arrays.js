60b5fafc7185767af7da8da9205174eaae414c07
Delfina Maria Fonseca <delfaop@gmail.com>
2021-03-29 13:51:58 -0300

    scripts

---
+++ Scripts/Arrays.js
@@ -0,0 +1,30 @@
+/* a */
+var months =["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+console.log(months[4], months[10])
+
+/* b */
+console.log(months.sort())
+
+/* c */
+months.unshift("delf")
+months.push("lilos")
+console.log(months)
+
+/* d */
+months.pop("delf")
+months.shift("lilos")
+console.log(months)
+
+/* e */
+months.reverse()
+console.log(months)
+
+/* f */
+console.log(months.join("-"))
+
+/* g */
+var months =["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+console.log(months.slice(4, 11))
+
+
+
---
+++ Scripts/Ifelse.js
@@ -0,0 +1,19 @@
+/* a */
+var number = Math.random()
+if (number > 0.4) {
+    alert("Greter than 0,5")
+} else if {
+    alert("Lower than 0,5")   
+};
+console.log(number)
+
+/* b */
+ var age = Math.floor(Math.random() * (100 - 0 + 1) ) + 0;
+   /* i */
+    if (age< 2) {
+        alert = "Bebe" 
+    
+    /* ii */
+    if (age< 13) {
+
+    }
